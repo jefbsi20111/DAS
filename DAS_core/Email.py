e943b961543978e44f4a0fe71f230157d745b20d
jefbsi20111 <jefbsi2011.1@hotmail.com>
2015-06-08 16:11:49 -0300

    fixtures, email, reset for db

--- DAS_core/Email.py
+++ DAS_core/Email.py
@@ -1,8 +1,6 @@
 from django.core.mail.message import EmailMessage
 from django.core.mail import send_mail
 
-
-#outra tentativa
 class das_email:
 	def enviar_email(self,assunto,corpo,de,para):
 		send_mail(assunto, corpo,de,[para])
