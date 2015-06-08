a0c0f77c8e02ac82f45f7cdeb9dbb576dadb159e
das <das@das.(none)>
2015-06-08 15:30:30 -0300

    todo o resto

--- DAS_core/views.py
+++ DAS_core/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import Context, loader
+from django.http import Http404
 
 # Create your views here.
+
+
+def error404(request):
+	
+    return render(request, 'templates/home.html',status=404)
+    
--- src_DAS/urls.py
+++ src_DAS/urls.py
@@ -1,5 +1,11 @@
 from django.conf.urls import patterns, include, url
 
+from django.conf.urls.static import static
+from django.conf.urls import handler404
+from django.conf.urls import patterns, include, url
+from django.http import Http404
+
+
 from django.contrib import admin
 admin.autodiscover()
 
